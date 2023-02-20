da4a12fa0b0ddc9803b7213cc363d5c15c8b9358
tujh <somenkoea@gmail.com>
2023-02-20 12:57:12 +0300

    config worker, provider

--- config/config.go
+++ config/config.go
@@ -1,12 +1,16 @@
 package config
 
-import "os"
+import (
+	"os"
+	"time"
+)
 
 type (
 	Config struct {
 		App
 		Log
 		DB
+		Workers []Worker
 	}
 
 	App struct {
@@ -19,6 +23,18 @@ type (
 
 	DB struct {
 		URL string
+	}
+
+	Worker struct {
+		ID           string
+		PollInterval string // todo to duration
+		Provider     Provider
+	}
+
+	Provider struct {
+		ID          string
+		BaseUrl     string
+		HttpTimeout time.Duration
 	}
 )
 
